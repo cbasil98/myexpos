d1c8f952819b93ab9eacdae22859c308057ad0a8
cbasil98 <cbasil98@gmail.com>
2019-09-01 03:26:32 +0530

    stage 12

---
+++ xfs-interface/cmd.bat
@@ -0,0 +1,4 @@
+load --os /home/basil/myexpos/spl/spl_progs/OScode.xsm
+load --idle /home/basil/myexpos/expl/expl_progs/idle.xsm
+load --int=timer /home/basil/myexpos/spl/spl_progs/timer.xsm
+load --init /home/basil/myexpos/expl/expl_progs/init.xsm
