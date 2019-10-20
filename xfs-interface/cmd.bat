ec241c3dde9d6645dd573d70b04c3a0fbfd8c177
cbasil98 <cbasil98@gmail.com>
2019-10-21 01:40:19 +0530

    Stage 16

--- xfs-interface/cmd.bat
+++ xfs-interface/cmd.bat
@@ -7,3 +7,6 @@ load --module 5 /home/basil/myexpos/spl/spl_progs/contextswitchmodule.xsm
 load --int=10 /home/basil/myexpos/spl/spl_progs/INT10.xsm
 load --module 0 /home/basil/myexpos/spl/spl_progs/module0.xsm
 load --module 4 /home/basil/myexpos/spl/spl_progs/module4.xsm
+load --int=6 /home/basil/myexpos/spl/spl_progs/INT6.xsm
+load --int=console /home/basil/myexpos/spl/spl_progs/consoleinterrupthandler.xsm
+load --exhandler /home/basil/myexpos/spl/spl_progs/exchand.xsm
